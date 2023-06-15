8e846cf2fd95bd19d27ad8b68ff7be14093931db
inin15 <101159760+inin15@users.noreply.github.com>
2023-06-15 13:53:55 +0700

    codelearn ass4+ass5
    code learn ass4+ass5

---
+++ Net.M.A004/Net.M.A004/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Net.M.A004
+{
+    internal class Program
+    {
+        private int x = 1;
+
+        private static void Main(string[] args)
+        {
+            Program a = new Program();
+            Console.WriteLine($"x = {a.x}");
+            Console.WriteLine($"y = {a.Calculate()}");
+        }
+
+        public int Calculate()
+        {
+            return (2 * x * x * x - 6 * x * x + 2 * x - 1);
+        }
+    }
+}
---
+++ Net.M.A005/Net.M.A005.Exercise3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Net.M.A005.Exercise3
+{
+    internal class Program
+    {
+        public static bool CheckPrimeNumber(int n)
+        {
+            // so nguyen n < 2 khong phai la so nguyen to
+            if (n < 2)
+            {
+                return false;
+            }
+            // check so nguyen to khi n >= 2
+            int squareRoot = (int)Math.Sqrt(n);
+            int i;
+            for (i = 2; i <= squareRoot; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int.TryParse(Console.ReadLine(), out int n);
+            if (CheckPrimeNumber(n))
+            {
+                Console.WriteLine($"{n} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not a Prime Number", n);
+            }
+        }
+    }
+}
