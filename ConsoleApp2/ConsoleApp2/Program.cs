6634f2988a7ad0a2c2b8cc690d9210ea65cc1b74
inin15 <101159760+inin15@users.noreply.github.com>
2023-06-15 09:33:48 +0700

    code learn ass1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assingment1
+
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string a = "Hello";
+            Console.WriteLine(a);
+            Console.Write("Please enter your name :");
+            string b = Console.ReadLine();
+            Console.WriteLine("Hello {0}", b);
+        }
+    }
+}
